cb06c0cfd607a5ddb49268bb3a6872e35a614221
emiliocorpus <emiliocorpus@gmail.com>
2016-10-22 21:56:10 -0700

    plays around with reddit build

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def reddit
+    binding.pry
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    binding.pry
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Reddit") if is_navigational_format?
+    else
+      binding.pry
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'callbacks/reddit'
   root 'welcome#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
