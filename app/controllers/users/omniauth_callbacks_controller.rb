78bf25b54d09f74d240555a713fb6c7f8b276250
emiliocorpus <emiliocorpus@gmail.com>
2016-10-24 16:37:05 -0700

    generates changes for user auth, adds views

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def reddit
-    binding.pry
     @user = User.from_omniauth(request.env["omniauth.auth"])
     binding.pry
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Reddit") if is_navigational_format?
-    else
+      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      # set_flash_message(:notice, :success, :kind => "Reddit") if is_navigational_format?
+
+      sign_in @user
+      redirect_to root_path
     end
   end
 end
