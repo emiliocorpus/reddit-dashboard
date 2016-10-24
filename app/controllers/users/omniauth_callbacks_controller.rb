d6027cfa7e9f4541569196a9cc30be3864e8c027
emiliocorpus <emiliocorpus@gmail.com>
2016-10-24 16:24:29 -0700

    changes for reddit oauth2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Reddit") if is_navigational_format?
     else
-      binding.pry
     end
   end
 end
