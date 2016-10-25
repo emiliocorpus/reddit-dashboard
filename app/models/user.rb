63785a1a12f56878d330fb3dc20db302b047e3ca
emiliocorpus <emiliocorpus@gmail.com>
2016-10-24 23:07:54 -0700

    adds comments for formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,14 @@ class User < ActiveRecord::Base
   end
 
 
-  #   headers = {"Authorization": "bearer #{token}"}
-  #   response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+  #   headers = {"Authorization": "bearer #{token}", "User-Agent": "RedditWebDashboard/0.1 by Emilio Corpus"}
+  #   query = {:limit => 50, :count => #{NUMBER OF ITEMS YOU HAVE RETRIEVED}}, :after/before => ["name"] of the listing i.e. t3_g7aj2
+
+  #     THIS WILL GET THE FRONT PAGE OF WHICHEVER USER AUTHORIZES IT
+  #   response = HTTParty.get("https://oauth.reddit.com/", :headers => headers, :query => query)
+
+
+
 
   def email_required?
     false
