998e992e7fed8cbdf45ab19f4dee80c39efc73b3
emiliocorpus <emiliocorpus@gmail.com>
2016-10-25 11:12:56 -0700

    completes basic navbar with sign_in and sign_out

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     if current_user
-      current_client = RedditClient.new(current_user)
-      binding.pry
+      # current_client = RedditClient.new(current_user)
+      # binding.pry
     end
   end
 end
