0558d14fb074f63feec5fe04a53a531584ef447f
emiliocorpus <emiliocorpus@gmail.com>
2016-10-25 11:23:41 -0700

    prime for pull from master

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with no current user signed in' do
+    it 'should have a navbar with a login link' do
+      visit root_path
+      expect(response.body).to have_link("Sign in With Reddit")
+    end
+  end
 end
