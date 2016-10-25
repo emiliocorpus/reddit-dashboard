ebd07c7d2b62ef9adb30fad1eaab057ce13c4d67
emiliocorpus <emiliocorpus@gmail.com>
2016-10-24 19:01:45 -0700

    updates app for custom classes/modules

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module RedditDashboard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.autoload_paths += %W(#{config.root}/lib)
     config.generators.assets = false
     config.generators.helper = false
   end
