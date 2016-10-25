543f5c5bf4e7bce889aa82dbeb305d927207ebc7
emiliocorpus <emiliocorpus@gmail.com>
2016-10-25 11:01:50 -0700

    adds empty refresh_access method

--- lib/reddit_client.rb
+++ lib/reddit_client.rb
@@ -17,15 +17,29 @@ class RedditClient
     binding.pry
   end
 
+
+
+  def refresh_access
+    # METHOD TO REFRESH TOKEN WHEN RESPONSE  = 401 Unauthorized
+  end
+
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
 # DateTime.strptime("1477418075",'%s')
-
 # :body => {:client_id => ENV["reddit_client_id"], :client_secret => ENV["reddit_client_secret"], :redirect_uri=>"http://localhost:3000/users/auth/reddit/callback"}
-
-
 #   headers = {"Authorization": "bearer #{token}", "User-Agent": "RedditWebDashboard/0.1 by Emilio Corpus"}
 #   query = {:limit => 50, :count => #{NUMBER OF ITEMS YOU HAVE RETRIEVED}}, :after/before => ["name"] of the listing i.e. t3_g7aj2 }
-
 #     THIS WILL GET THE FRONT PAGE OF WHICHEVER USER AUTHORIZES IT
 #   response = HTTParty.get("https://oauth.reddit.com/", :headers => headers, :query => query)
