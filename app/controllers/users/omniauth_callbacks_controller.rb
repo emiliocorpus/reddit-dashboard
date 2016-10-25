d3a206a2ff40f3210708be8116bb00e09e867194
emiliocorpus <emiliocorpus@gmail.com>
2016-10-24 23:39:51 -0700

    adds logout link to root page

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def reddit
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      binding.pry
       # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       # set_flash_message(:notice, :success, :kind => "Reddit") if is_navigational_format?
       sign_in @user
