63c777ec058086863a1aa02972742adbeab85790
emiliocorpus <emiliocorpus@gmail.com>
2016-10-25 11:46:34 -0700

    first passing tests index.html.erb_rspec

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", type: :view do
   describe 'with no current user signed in' do
+
     it 'should have a nav element' do
-      expect(response.body).to have_css('nav')
+      visit '/'
+      page.has_css?('nav')
     end
     it 'should have a navbar with a login link' do
-      visit root_path
-      expect(response.body).to have_link("Sign in With Reddit")
+      visit '/'
+      find_link('Sign in With Reddit').visible?
     end
   end
 end
